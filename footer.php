4ceadeed2551891d9fa647a7e89a12060ef3f9d9
Giancarlo Galliani <galliani.08@gmail.com>
2016-12-11 13:13:53 +0000

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Bisi\'s_Wordpress_Theme
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bisis-wordpress-theme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'bisis-wordpress-theme' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'bisis-wordpress-theme' ), 'bisis-wordpress-theme', '<a href="http://www.ggalliani.com" rel="designer">Giancarlo Galliani</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
